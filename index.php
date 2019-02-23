4fa33a5c9c468b297e862a242abe65917985ebcb
soruly <soruly@gmail.com>
2019-02-23 16:40:35 +0000

    Add secure prefix to php session cookies

--- index.php
+++ index.php
@@ -12,6 +12,7 @@ else{
 
 ini_set( 'session.cookie_httponly', true);
 ini_set( 'session.cookie_secure', true);
+session_name('__Secure-PHPSESSID');
 session_start();
 
 if(!isset($_SESSION["token"]) && isset($_COOKIE["password"])){
